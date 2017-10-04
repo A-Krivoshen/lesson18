c5ebfa604a9790afed05b1bcf50b6d1c32db9c54
A-Krivoshein <a.krivoshein@gmail.com@>
2017-10-04 21:33:36 +0300

    ya geo

--- index.php
+++ index.php
@@ -19,15 +19,23 @@ if (!empty($_GET['find'])) {
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
-    <title>5.1</title>
+    <title>geo</title>
 </head>
-<body>
+<body style="background-color: lightgoldenrodyellow;">
 <div class="container">
+    <div id="map" class="col-md-6 offset-md-3"
+         style="height: 300px; margin-top: 40px; padding: 0; border: 1px solid rgba(0,0,0,.165);"></div>
+
     <form method="get" class="container">
-        <div>
-            <div>
-                <div>
+        <div class="row">
+            <div style="margin-top: 40px" class="col-md-6 offset-md-3">
+                <div class="input-group">
                   <span class="input-group-btn">
                     <button class="btn btn-secondary" type="submit">Найти</button>
                   </span>
@@ -38,17 +46,19 @@ if (!empty($_GET['find'])) {
     </form>
 
     <?php if (!empty($array)): ?>
-        <div>
+        <div class="col-md-6 offset-md-3" style="margin-bottom: 40px;">
             <?php foreach ($array as $item): ?>
                 <a href="<?php echo 'index.php?find=' . urlencode($_GET['find']) . '&result=' . urlencode($item['pos']); ?>"
-                   >
-                    <h4><?php echo $item['pos'] ?></h4>
-                    <p><?php echo $item['address'] ?></p>
+                   class="list-group-item result">
+                    <h4 class="list-group-item-heading col-md-12"><?php echo $item['pos'] ?></h4>
+                    <p class="list-group-item-text col-md-12" style="margin-bottom: 0;"><?php echo $item['address'] ?></p>
                 </a>
             <?php endforeach; ?>
         </div>
     <?php endif; ?>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script src="index.js"></script>
 </body>
 </html>
